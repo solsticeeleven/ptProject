7a15d8548ff575c73abbf5b8d2e2ba52b4d1a8a1
AhmedWaleed <ahmed@WILLY-S>
2025-11-21 01:28:53 +0200

    fixed the design tool bar problem that the bottom images keeps disapearing after 4 clicks added code that repaints the bottom toolbar images (the same images you already use in CreateDesignToolBar) after clearing the drawing area.

--- GUI/Output.cpp
+++ GUI/Output.cpp
@@ -86,10 +86,79 @@ void Output::ClearStatusBar()const
 //Clears the drawing (degin) area
 void Output::ClearDrawingArea() const
 {
+	// Clear the main drawing area (between top toolbar and status bar)
 	pWind->SetPen(RED, 1);
 	pWind->SetBrush(WHITE);
 	pWind->DrawRectangle(0, UI.ToolBarHeight, UI.width, UI.height - UI.StatusBarHeight);
-	
+
+	// --- Ensure bottom toolbar images stay visible ---
+	// Repaint the bottom toolbar area and its images (only bottom row)
+	// This avoids toolbar icons being overwritten by subsequent drawing operations.
+
+	// Prepare list of images for each menu item (matches DsgnMenuItem order)
+	string MenuItemImages[ITM_DSN_CNT];
+	MenuItemImages[ITM_AND2]  = "images\\Menu\\Menu_AND2.jpg";
+	MenuItemImages[ITM_OR2]   = "images\\Menu\\Menu_OR2.jpg";
+	MenuItemImages[ITM_NAND2] = "images\\Menu\\Menu_NAND2.jpg";
+	MenuItemImages[ITM_NOR2]  = "images\\Menu\\Menu_NOR2.jpg";
+	MenuItemImages[ITM_XOR2]  = "images\\Menu\\Menu_XOR2.jpg";
+	MenuItemImages[ITM_XNOR2] = "images\\Menu\\Menu_XNOR2.jpg";
+	MenuItemImages[ITM_AND3]  = "images\\Menu\\Menu_AND3.jpg";
+	MenuItemImages[ITM_NOR3]  = "images\\Menu\\Menu_NOR3.jpg";
+	MenuItemImages[ITM_XOR3]  = "images\\Menu\\Menu_XOR3.jpg";
+	MenuItemImages[ITM_Buff]  = "images\\Menu\\Menu_Buff.jpg";
+	MenuItemImages[ITM_INV]   = "images\\Menu\\Menu_INV.jpg";
+	MenuItemImages[ITM_SWITCH]= "images\\Menu\\Menu_SWITCH.jpg";
+	MenuItemImages[ITM_LED]   = "images\\Menu\\Menu_LED.jpg";
+	MenuItemImages[ITM_CONNECTION] = "images\\Menu\\Menu_CONNECTION.jpg";
+
+	MenuItemImages[ITM_LABEL] = "images\\Menu\\Menu_Label.jpg";
+	MenuItemImages[ITM_EDIT]  = "images\\Menu\\Menu_Edit.jpg";
+	MenuItemImages[ITM_DEL]   = "images\\Menu\\Menu_Del.jpg";
+	MenuItemImages[ITM_MOVE]  = "images\\Menu\\Menu_Move.jpg";
+	MenuItemImages[ITM_SAVE]  = "images\\Menu\\Menu_Save.jpg";
+	MenuItemImages[ITM_LOAD]  = "images\\Menu\\Menu_Load.jpg";
+	MenuItemImages[ITM_SIM_MODE] = "images\\Menu\\Menu_SIM_MODE.jpg";
+	MenuItemImages[ITM_EXIT]  = "images\\Menu\\Menu_Exit.jpg";
+
+	// bottom row indices (AND2 .. CONNECTION)
+	const int bottomStart = ITM_AND2;
+	const int bottomEnd   = ITM_CONNECTION;
+	const int bottomCount = bottomEnd - bottomStart + 1;
+
+	int bottomSlotW = (bottomCount > 0) ? (UI.width / bottomCount) : UI.width;
+	int slotH = UI.ToolBarHeight;
+	int bottomY = UI.height - UI.StatusBarHeight - UI.ToolBarHeight;
+
+	// draw bottom background (overwrite any artifacts)
+	pWind->SetPen(UI.BkGrndColor);
+	pWind->SetBrush(UI.BkGrndColor);
+	pWind->DrawRectangle(0, bottomY, UI.width, bottomY + slotH);
+
+	// draw bottom row images (AND2 .. CONNECTION)
+	for (int i = 0; i < bottomCount; ++i) {
+		int itemIndex = bottomStart + i;
+		const string &path = MenuItemImages[itemIndex];
+
+		int imgW = (bottomSlotW * 80) / 100;
+		int imgH = (slotH * 80) / 100;
+		int xPos = i * bottomSlotW + (bottomSlotW - imgW) / 2;
+		int yPos = bottomY + (slotH - imgH) / 2;
+
+		if (!path.empty() && FileExists(path)) {
+			try {
+				pWind->DrawImage(path, xPos, yPos, imgW, imgH);
+			} catch (...) {
+				pWind->SetPen(UI.BkGrndColor);
+				pWind->SetBrush(UI.BkGrndColor);
+				pWind->DrawRectangle(i * bottomSlotW, bottomY, (i + 1) * bottomSlotW, bottomY + slotH);
+			}
+		} else {
+			pWind->SetPen(UI.BkGrndColor);
+			pWind->SetBrush(UI.BkGrndColor);
+			pWind->DrawRectangle(i * bottomSlotW, bottomY, (i + 1) * bottomSlotW, bottomY + slotH);
+		}
+	}
 }
 //////////////////////////////////////////////////////////////////////////////////////////
 //Draws the menu (toolbar) in the Design mode
@@ -358,7 +427,7 @@ void Output::DrawXOR2(GraphicsInfo r_GfxInfo, bool selected) const
 {
 	string GateImage;
 	if (selected)
-		GateImage = "Images\\Gates\\Gate_XOR2_Hi.jpg";
+		GateImage="Images\\Gates\\Gate_XOR2_Hi.jpg";
 	else
 		GateImage = "Images\\Gates\\Gate_XOR2.jpg";
 	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
